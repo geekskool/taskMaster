253cf40e4e8501f5eb91d9e142617f8def7c16e3
Anirudh B S <anirudhbs96@gmail.com>
2017-09-23 20:17:07 +0530

    Adds POST method for createTask pug file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
 
 const path = require('path')
 const routes = require('./myRoutes.js')
 const tasks = require('./data/tasks');
 app.set('views', path.join(__dirname, 'public/views'))
 app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.get('/', function (req, res) {
   res.send('something')
@@ -19,23 +23,39 @@ app.get(routes.register, function (req, res) {
   res.send('Register Page')
 })
 
-app.get(routes.dashboard, function(req, res) {
+app.get(routes.dashboard, function (req, res) {
   res.render('dashboard', tasks);
 });
 
-app.get(routes.createtask, function(req, res){
+app.get(routes.createtask, function (req, res) {
   res.render('createtask', {})
 })
 
+app.post(routes.createtask, function (req, res) {
+  res.send({
+    name: req.body.title,
+    desc: req.body.description,
+    assignedto: req.body.assignedTo,
+    duedate: req.body.dueDate
+  })
+})
+
 app.get(routes.tasks, function (req, res) {
-  res.render('tasks', {title: 'tasks', message: 'Hello', tasks: tasks})
+  res.render('tasks', {
+    title: 'tasks',
+    message: 'Hello',
+    tasks: tasks
+  })
 })
 
 app.get(routes.task, function (req, res) {
-//  res.sendFile('Ind Task Page'+ req.params.id);
+  //  res.sendFile('Ind Task Page'+ req.params.id);
 
-//  res.sendFile('static/html/task.html', {root: __dirname} )
-  res.render('task', {task_id:req.params.id, task_title:'task one'})
+  //  res.sendFile('static/html/task.html', {root: __dirname} )
+  res.render('task', {
+    task_id: req.params.id,
+    task_title: 'task one'
+  })
 
 });
 
