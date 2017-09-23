362f032dbf512cfac38cd108874440ab4242f3ed
Alen Thomas <alenthomas9@gmail.com>
2017-09-23 18:30:04 +0530

    Add new dummy data

--- data/tasks.js
+++ data/tasks.js
@@ -1,24 +1,141 @@
-const tasks = {
-  'user': 'alen',
-  'tasks': [
-   {
-    'tskId': 1234,
-    'title': 'some sting',
-    'desc': 'task desc',
-    'status': 'todo',
-    'assgnBy': 'boss',
-    'createdOn': '2017-09-23T09:47:48',
-    'dueDate': '2017-09-23T09:47:48'
+const data = {
+  tasks: {
+    1234: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'todo',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+    },
+    5678: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'new',
+      assgnBy: 'boss',
+      assgnTo: 'xyz',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+    },
+    78980: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'inprogress',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
    },
-   {
-    'tskId': 234,
-    'title': 'task 2',
-    'desc': 'task 2 desc',
-    'status': 'todo',
-    'assgnBy': 'boss',
-     'createDate': '2017-09-23T09:47:48',
-    'dueDate': '2017-09-23T09:47:48'
-  }]
+    89876: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'closed',
+      assgnBy: 'boss',
+      assgnTo: 'xyz',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    45678: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'cancelled',
+      assgnBy: 'xyz',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    876: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'todo',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    2345: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'new',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    456234: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'inprogress',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    456876: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'cancelled',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    98345: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'inprogress',
+      assgnBy: 'boss',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    76567: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'closed',
+      assgnBy: 'mukesh',
+      assgnTo: 'abc',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    98345: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'new',
+      assgnBy: 'adarsh',
+      assgnTo: 'donna',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    5687: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'inprogress',
+      assgnBy: 'mrinal',
+      assgnTo: 'maneesh',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    854367: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'cancelled',
+      assgnBy: 'alen',
+      assgnTo: 'anirudh',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+   },
+    5467: {
+      title: 'some sting',
+      desc: 'task desc',
+      status: 'new',
+      assgnBy: 'pavan',
+      assgnTo: 'manoj',
+      createdOn: '2017-09-23T09:47:48',
+      dueDate: '2017-09-23T09:47:48'
+    }
+  }
 }
 
-module.exports = tasks
+module.exports = data
