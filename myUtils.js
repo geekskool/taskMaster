ec8a3fc108283da5f8993850f41db2d42c94f3f2
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-09-24 07:34:57 +0530

    Add hashing support

--- myUtils.js
+++ myUtils.js
@@ -1,27 +1,32 @@
+let bcrypt = require('bcrypt')
 function sanitize (form) {
-  let keys = Object.keys(form);
-  let val = keys.filter((k) => form[k] !== '' );
-  if(keys.length == val.length)
-    return true
+  let keys = Object.keys(form)
+  let val = keys.filter((k) => form[k] !== '')
+  if (keys.length == val.length) { return true }
   return false
 };
 
-function authenticate(username, password) {
-  if(username === 'alen' && password === 'thomas')
-    return true
+function authenticate (username, password) {
+  if (username === 'alen' && password === 'thomas') { return true }
   return false
 }
 
-function displayError(errMsg, errData) {
-  if (errData)
-    return {error_message: errMsg, data: errData}
+function displayError (errMsg, errData) {
+  if (errData) { return {error_message: errMsg, data: errData} }
   return {error_message: errMsg}
 }
-
+function hashPassword (password) {
+  let salt = bcrypt.genSaltSync(10)
+  let hash = bcrypt.hashSync(password, salt)
+  return hash
+}
+function compareHash (password, hash) {
+  return bcrypt.compareSync(password, hash) === true
+}
 const utilFuns = {
   sanitize: sanitize,
   authenticate: authenticate,
   displayError: displayError
 }
 
-module.exports = utilFuns;
+module.exports = utilFuns
